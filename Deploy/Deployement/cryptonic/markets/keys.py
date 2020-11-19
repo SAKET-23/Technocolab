17f63c595518f986d18d445e6d016d1dc89c335a
SAKET-23 <69591071+SAKET-23@users.noreply.github.com>
2020-11-19 12:55:41 +0530

    Add files via upload

---
+++ Deploy/Deployement/cryptonic/markets/keys.py
@@ -0,0 +1,6 @@
+
+def Api():
+  return 'SWhWaHUfpfHt0ftrphLRshtmsRSF7vxKNB1j47wWADLJ2cySHiHuFOLuK5EQzTE5'
+
+def Secrets():
+    return 'sHSTEPLWWV70tb0TldnNJhfDBmRXeJwlqELLf4MGYChVCTHHAWittL958w3G5TdJ'
